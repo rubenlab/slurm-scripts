fdfc67c203a42dd51cf7daf38e26d7bf67c516f0
Tapu Shaikh <shaikh@gwdu108.global.gwdg.cluster>
2022-07-13 16:26:20 +0200

    added nemotoc

---
+++ nemotoc/nemotoc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+#SBATCH -D ./
+#SBATCH -J nemotoc
+#SBATCH -C scratch
+
+#SBATCH --partition=gpu
+#SBATCH -c 24
+#SBATCH --gres=gpu:1
+#SBATCH --time=59:00
+#SBATCH --qos=short
+
+#SBATCH -o slurm-nemotoc-%j.out
+#SBATCH -e slurm-nemotoc-%j.err
+
+source /usr/users/rubsak/sw/nemotoc/nemotoc.bashrc
+
+nemotocRun -c conf.py
+
