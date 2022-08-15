31db8d5533e68db93cf4e2f8e33d5132dad9ce6b
Tapu Shaikh <shaikh@gwdu108.global.gwdg.cluster>
2022-08-15 15:20:46 +0200

    upgraded to CryoCARE 0.2

---
+++ cryocare/cryocare_0-2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+#SBATCH -D ./
+#SBATCH -J cryoCARE
+#SBATCH -C scratch
+
+#SBATCH --partition=gpu
+#SBATCH --mem-per-cpu 6000 
+#SBATCH --gres=gpu:2
+#SBATCH --time=05:00:00
+
+#SBATCH -o slurm-cryocare-%j.out
+#SBATCH -e slurm-cryocare-%j.err
+
+module purge
+module load cuda/10.2.89
+#export KERAS_BACKEND=tensorflow  # NEEDED?
+
+source /usr/users/rubsak/sw/conda/conda_rubsak.bash cryocare_0.2_c10_nokeras
+
+module list
+echo "Conda evironment: $(basename $CONDA_DEFAULT_ENV)"
+
+cryoCARE_extract_train_data.py --conf train_data_config.json && \
+cryoCARE_train.py --conf train_config.json && \
+cryoCARE_predict.py --conf predict_config.json
+
