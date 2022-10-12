8d3e48fbd66da20100dc687c1f40c6d42fd0ae39
Tapu Shaikh <shaikh@gwdu108.global.gwdg.cluster>
2022-10-12 09:45:29 +0200

    removed path diagnostic

--- relion/relion4_qsub_cpu_sif.sh
+++ relion/relion4_qsub_cpu_sif.sh
@@ -23,7 +23,6 @@ function which() {
   echo "${sif_exe}"
 }
 
-echo "Executable: '`which relion_refine_mpi`'"
 echo -e "$(hostname) modules: $(module list 2>&1 | grep openmpi --color=never) \n"
 
 singularity exec --bind /scratch1/projects/rubsak --bind /local ${sif_path} mpirun XXXcommandXXX
--- relion/relion4_qsub_gpu_sif.sh
+++ relion/relion4_qsub_gpu_sif.sh
@@ -25,7 +25,6 @@ function which() {
   echo "${sif_exe}"
 }
 
-echo "Executable: '`which relion_refine_mpi`'"
 echo -e "$(hostname) modules: $(module list 2>&1 | grep openmpi --color=never) \n"
 
 singularity exec --nv --bind /scratch1/projects/rubsak --bind /local ${sif_path} mpirun XXXcommandXXX
