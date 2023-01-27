79e3b167a6a77afaea149b6ea9bc3f949fe75d06
Tapu Shaikh <shaikh@gwdu108.global.gwdg.cluster>
2023-01-27 18:01:21 +0100

    added ModelAngelo example

---
+++ model_angelo/model_angelo_submission.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+#SBATCH -D ./
+#SBATCH -J ModelAngelo
+#SBATCH -C scratch
+
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --time=2:00:00
+#SBATCH --qos=short
+
+#SBATCH -o modelangelo-%j.out
+#SBATCH -e modelangelo-%j.err
+
+module purge
+module load cuda/11.2
+shopt -s expand_aliases
+source /usr/users/rubsak/sw/rubsak.bashrc
+use_modelangelo
+
+model_angelo build -f human_asyn_full.fasta -v J61_postprocess_bottom.mrc -o J61_pp_bottom
+
